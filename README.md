bf56a8618b92244ecaf152400ca5d3ac18dd5512
Ben Croker <ben@putyourlightson.com>
2024-08-29 07:08:46 +0200

    Add configurable cut-off date

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# Clear Browser Cache Extension for Craft CMS
+# Clear Browser Cache Module for Craft CMS
 
-This package clears the local browser cache once per unique visitor. It does so by send a `Clear-Site-Data` response header with a value of `"cache"`, and sets a cookie called `BrowserCacheCleared` to prevent the header from being sent again.
+This module clears the local browser cache once per unique visitor. It does so by send a `Clear-Site-Data` response header with a value of `"cache"`, and sets a cookie called `BrowserCacheCleared` to prevent the header from being sent again.
 
-[Read this article](https://putyourlightson.com/articles/critical-update-for-a-blitz-blunder) to learn the reason for the existence of this package.
+To learn the reason for its existence, [read this article →](https://putyourlightson.com/articles/critical-update-for-a-blitz-blunder)
 
-Install this package via composer.
+Install this module via composer.
 
 ```shell
 composer require putyourlightson/craft-clear-browser-cache
+```
+
+Once installed, the module self-bootstraps and does not require any further setup.
+
+The module is configured to run only until a cut-off date, which is set to `2026-01-01` by default. After that date, the module will have no effect. This value can be overridden by setting a `CLEAR_BROWSER_CACHE_CUTOFF_DATE` environment variable in your `.env` file.
+
+```shell
+CLEAR_BROWSER_CACHE_CUTOFF_DATE=2025-09-01
 ```
 
 ---
--- src/ClearBrowserCache.php
+++ src/ClearBrowserCache.php
@@ -3,6 +3,7 @@
 namespace putyourlightson\clearbrowsercache;
 
 use Craft;
+use craft\helpers\App;
 use yii\base\BootstrapInterface;
 use yii\base\Event;
 use yii\base\Module;
@@ -12,30 +13,56 @@ use yii\web\Response;
 class ClearBrowserCache extends Module implements BootstrapInterface
 {
     /**
+     * The unique ID of this module.
+     */
+    public const ID = 'clear-browser-cache';
+
+    /**
+     * The default cut-off date for clearing the browser cache.
+     */
+    public const DEFAULT_CUTOFF_DATE = '2026-01-01';
+
+    /**
      * @inheritdoc
      */
     public function bootstrap($app): void
     {
-        if (Craft::$app->getRequest()->getIsSiteRequest()) {
-            Event::on(
-                Response::class,
-                Response::EVENT_AFTER_PREPARE,
-                function(Event $event) {
-                    $cookie = Craft::$app->getRequest()->getCookies()->get('BrowserCacheCleared');
-                    if ($cookie !== null) {
-                        return;
-                    }
+        Craft::$app->setModule(self::ID, $this);
 
-                    /** @var Response $response */
-                    $response = $event->sender;
-                    $response->getHeaders()->set('Clear-Site-Data', '"cache"');
-                    $response->getCookies()->add(new Cookie([
-                        'name' => 'BrowserCacheCleared',
-                        'value' => 1,
-                        'expire' => time() + (60 * 60 * 24 * 365 * 10), // 10 years
-                    ]));
-                }
-            );
+        if (Craft::$app->getRequest()->getIsSiteRequest()) {
+            $expireTime = $this->getExpireTime();
+            if ($expireTime > 0) {
+                Event::on(
+                    Response::class,
+                    Response::EVENT_AFTER_PREPARE,
+                    [$this, 'handleResponse'],
+                );
+            }
         }
+    }
+
+    private function getExpireTime(): int
+    {
+        $cutoffDate = App::env('CLEAR_BROWSER_CACHE_CUTOFF_DATE') ?? self::DEFAULT_CUTOFF_DATE;
+        $expireTime = strtotime($cutoffDate) - time();
+
+        return max($expireTime, 0);
+    }
+
+    private function handleResponse(Event $event): void
+    {
+        $cookie = Craft::$app->getRequest()->getCookies()->get('BrowserCacheCleared');
+        if ($cookie !== null) {
+            return;
+        }
+
+        /** @var Response $response */
+        $response = $event->sender;
+        $response->getHeaders()->set('Clear-Site-Data', '"cache"');
+        $response->getCookies()->add(new Cookie([
+            'name' => 'BrowserCacheCleared',
+            'value' => 1,
+            'expire' => $this->getExpireTime(),
+        ]));
     }
 }
